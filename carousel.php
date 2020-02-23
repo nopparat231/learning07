840f16164df1e21001eee3fec91348737499f4df
nopparat231 <23.noop@gmail.com>
2020-02-23 12:31:09 +0700

    add carousel

--- carousel.php
+++ carousel.php
@@ -12,7 +12,7 @@
       <img class="d-block w-100" src="img/b2.png" alt="Second slide" height="250px">
     </div>
     <div class="carousel-item">
-      <img class="d-block w-100" src="img/b3.jpg" alt="Third slide" height="250px">
+      <img class="d-block w-100" src="img/b4.jpg" alt="Third slide" height="250px">
     </div>
   </div>
   <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
